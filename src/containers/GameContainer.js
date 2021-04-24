956366de56c1471624b34c62cdea1138f3120694
Stanhoucke <stanhoucke@gmail.com>
2021-04-24 18:40:37 +0100

    added state hooks for maze data

--- src/containers/GameContainer.js
+++ src/containers/GameContainer.js
@@ -8,6 +8,10 @@ const GameContainer = () => {
 
     const [mazeId, setMazeId] = useState("");
     const [mazeState, setMazeState] = useState({});
+    const [ponyPosition, setPonyPosition] = useState(null);
+    const [domokunPosition, setDomokunPosition] = useState(null);
+    const [endPointPosition, setEndPointPosition] = useState(null);
+    const [walls, setWalls] = useState(null);
     const [loaded, setLoaded] = useState(false);
 
     const initialRender = useRef(true);
@@ -34,7 +38,13 @@ const GameContainer = () => {
         console.log("getting maze state...")
         
         request.get(url + '/' + mazeId)
-        .then(data => setMazeState(data))
+        .then(data => {
+            setMazeState(data)
+            setPonyPosition(data.pony[0])
+            setDomokunPosition(data.domokun[0])
+            setEndPointPosition(data["end-point"][0])
+            setWalls(data.data)
+        })
         .then(() => setLoaded(true))
     }
 
