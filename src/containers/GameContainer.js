83fe3e8292442d367acce4624f9b405ad44f65ee
Stanhoucke <stanhoucke@gmail.com>
2021-04-24 16:16:03 +0100

    created api request helper functions

--- src/containers/GameContainer.js
+++ src/containers/GameContainer.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Request from './helpers/Request';
 import NewGame from '../components/NewGame';
 import Maze from '../components/Maze';
 import EndGame from '../components/EndGame';
 
 const GameContainer = () => {
+
+    const [maze, setMaze] = useState({});
+    const [loaded, setLoaded] = useState(false);
+
+    const url = "https://ponychallenge.trustpilot.com/pony-challenge/maze/"
+
+    
 
     return (
         <>
